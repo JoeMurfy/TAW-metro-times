086bfd1b84335633f5b52c67f65897d16ceb4795
JoeMurfy <joemurphybusiness@gmail.com>
2022-04-27 10:40:33 +0100

    add functions for getting times and formatting

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-import re
+#
+# Script Name: TAW Metro Times
+# Author: Joe Murphy - 19/04/2022
+#
 
+import re
 import requests
-import json
 from bs4 import BeautifulSoup
 
-url = 'https://www.nexus.org.uk/metro/timetables-and-stations/ilford-road'
+# Get train times for STATION from Nexus.org using requests, parse HTML and return.
+def get_train_times(station):
+    url = f'https://www.nexus.org.uk/metro/timetables-and-stations/{station}'
+    response = requests.get(url)
+    raw_train_times = BeautifulSoup(response.text, 'html.parser')
 
-response = requests.get(url)
+    # TODO Change IDs for different directions of travel and platforms.
+    # Find train times by their HTML ID. The second 'g' variable may not exist on the page if
+    # no more trains are running to that destination.
+    train_times_y_text = raw_train_times.find(id='platform_1_y').get_text()
+    train_times_g_text = raw_train_times.find(id='platform_1_g').get_text()
+    return train_times_y_text, train_times_g_text
 
-soup = BeautifulSoup(response.text, 'html.parser')
-
-train_times_south_y_text = soup.find(id='platform_1_y').get_text()
-train_times_south_g_text = soup.find(id='platform_1_g').get_text()
-
-time_format_pattern = "([01]?[0-9]|2[0-3]):([0-5][0-9])?"
-times = re.findall(time_format_pattern, train_times_south_y_text)
+# Return requested train times in a human readable format. This removes 'this train terminates at X' from
+# being added to the list of train times.
+def format_train_times(train_times):
+    time_24h_format_pattern = "([01]?[0-9]|2[0-3]):([0-5][0-9])?"
+    formatted_train_times = re.findall(time_24h_format_pattern, train_times)
+    return formatted_train_times
 
 if __name__ == '__main__':
-    # print(train_times_south_y_text)
-    # print(train_times_south_g_text)
-    print(times)
-
+    unformatted_ilford_road_train_times = get_train_times('ilford-road')
+    formatted_ilford_road_train_times = format_train_times(unformatted_ilford_road_train_times)
+    print(formatted_ilford_road_train_times)
